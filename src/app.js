8d03ab90fd2606d37799f04b44085100f17b618a
Diego Vergara <71197696+Ninewinger@users.noreply.github.com>
2022-01-24 20:12:56 -0300

    save01

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  //battleship game 9*9 on click on cell turn red if there is a ship and blue if there is no ship
+  // 0 = empty
+  // 1 = part of a ship
+  // 2 = a sunken part of a ship
+  // 3 = a missed shot
+  let gameBoard = [
+    [1,1,1,1,1,0,0,0,0,1],
+    [0,0,0,0,0,0,0,0,0,1],
+    [0,0,0,0,0,0,0,0,0,1],
+    [0,0,0,0,0,0,0,0,0,1],
+    [0,0,0,0,0,0,0,0,0,0],
+    [1,0,0,1,1,0,0,0,0,0],
+    [1,0,0,0,0,0,0,0,0,0],
+    [1,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [1,1,1,1,0,0,0,0,0,0]
+  ];
+  
+
   console.log("Hello Rigo from the console!");
 };
