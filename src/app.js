7708cd54c6043c71f7939e7dfa4a4495f023a657
Diego Vergara <71197696+Ninewinger@users.noreply.github.com>
2022-01-25 20:09:43 -0300

    yey

--- src/app.js
+++ src/app.js
@@ -1,11 +1,3 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-import { eventListeners } from "@popperjs/core";
-
 window.onload = function() {
 
 
@@ -98,16 +90,17 @@ window.onload = function() {
   // 3 = a missed shot
   let gameBoard = [
     ["","A","B","C","D","E","F","G","H","I"],
-    [1,1,1,1,1,1,0,0,0,0,1],
-    [2,0,0,0,0,0,0,0,0,0,1],
-    [3,0,0,0,0,0,0,0,0,0,1],
-    [4,0,0,0,0,0,0,0,0,0,1],
-    [5,0,0,0,0,0,0,0,0,0,0],
-    [6,1,0,0,1,1,0,0,0,0,0],
-    [7,1,0,0,0,0,0,0,0,0,0],
-    [8,1,0,0,0,0,0,0,0,0,0],
-    [9,1,1,1,1,0,0,0,0,0,0]
+    [1,1,1,1,1,1,0,0,0,1],
+    [2,0,0,0,0,0,0,0,0,1],
+    [3,0,0,0,0,0,0,0,0,1],
+    [4,0,0,0,0,0,0,0,0,1],
+    [5,0,0,0,0,0,0,0,0,0],
+    [6,1,0,1,1,0,0,0,0,0],
+    [7,1,0,0,0,0,0,0,0,0],
+    [8,1,0,0,0,0,0,0,0,0],
+    [9,1,1,1,1,0,0,0,0,0]
   ];
+  console.log(gameBoard[1][1]);
 
   function letterToNumber(letter) {
     if(letter === "A") {
@@ -132,48 +125,25 @@ window.onload = function() {
   }
 
   function gameBoardId(id) {
-    let lett = id.charAt(0);
+    let lett = id.slice(0,1);
     let col = letterToNumber(lett);
-    let row = id.charAt(1);
-    return gameBoard[row][col];
+    let row = id.slice(1,2);
+    return gameBoard[col][row];
   }  
   
-  cells.addEventListener("click", function() {
-    if(gameBoardId(A1) === 0) {
-      A1.className = "cellMiss";
-      gameBoard[1][1] = 3;
-    } else if(gameBoardId(A1) === 1) {
-      A1.className = "cellHit";
-      gameBoard[1][1] = 2;
-    }
-    if(gameBoardId(A2) === 0) {
-      A2.className = "cellMiss";
-      gameBoard[1][2] = 3;
-    } else if(gameBoardId(A2) === 1) {
-      A2.className = "cellHit";
-      gameBoard[1][2] = 2;
-    }
-    if(gameBoardId(A3) === 0) {
-      A3.className = "cellMiss";
-      gameBoard[1][3] = 3;
-    } else if(gameBoardId(A3) === 1) {
-      A3.className = "cellHit";
-      gameBoard[1][3] = 2;
-    }
-    if(gameBoardId(A4) === 0) {
-      A4.className = "cellMiss";
-      gameBoard[1][4] = 3;
-    } else if(gameBoardId(A4) === 1) {
-      A4.className = "cellHit";
-      gameBoard[1][4] = 2;
-    };
-    if(gameBoardId(A5) === 0) {
-      A5.className = "cellMiss";
-      gameBoard[1][5] = 3;
-    } else if(gameBoardId(A5) === 1) {
-      A5.className = "cellHit";
-      gameBoard[1][5] = 2;
-    }
+  //for each cell add an event listener
+  [A1, A2, A3, A4, A5, A6, A7, A8, A9, B1, B2, B3, B4, B5, B6, B7, B8, B9, C1, C2, C3, C4, C5, C6, C7, C8, C9, D1, D2, D3, D4, D5, D6, D7, D8, D9, E1, E2, E3, E4, E5, E6, E7, E8, E9, F1, F2, F3, F4, F5, F6, F7, F8, F9, G1, G2, G3, G4, G5, G6, G7, G8, G9, H1, H2, H3, H4, H5, H6, H7, H8, H9, I1, I2, I3, I4, I5, I6, I7, I8, I9].forEach((e) => {
+    e.addEventListener("click", (event) => {
+      let id = event.target.id;
+      let cell = gameBoardId(id);
+      if(cell === 0) {
+        event.target.className = "cellMiss";
+        gameBoard[id.slice(1,2)][id.slice(0,1)] = 3;
+      } else if(cell === 1) {
+        event.target.className = "cellHit";
+        gameBoard[id.slice(1,2)][id.slice(0,1)] = 2;
+      }
+    });
   });
-
+  
 };
